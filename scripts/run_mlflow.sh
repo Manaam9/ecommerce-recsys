1ed68508ddfb7febfd3d6fa6f6ff9a66a79e9b0a
Manaam9 <manaam@yandex.ru>
2026-04-11 01:47:03 +0300

    обновил скрипт для запуска mlflow

--- scripts/run_mlflow.sh
+++ scripts/run_mlflow.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
 echo "Starting MLflow..."
 
-mlflow server \
+# переходим в папку mlflow
+mkdir -p mlflow
+cd mlflow
+
+# создаём папку для артефактов
+mkdir -p mlruns
+
+# запуск через python (чтобы не было проблем с PATH)
+python -m mlflow server \
   --backend-store-uri sqlite:///mlflow.db \
   --default-artifact-root ./mlruns \
-  --host 0.0.0.0 \
+  --host 127.0.0.1 \
   --port 5000
   
